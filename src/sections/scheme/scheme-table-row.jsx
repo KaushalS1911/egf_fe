37f47624185665d0a6d6d449ea1d79b0a4de3eef
DARSHIL <darshilthummar01@gmail.com>
2024-10-14 14:30:39 +0530

    gold price calculation for rpg

--- src/sections/scheme/scheme-table-row.jsx
+++ src/sections/scheme/scheme-table-row.jsx
@@ -60,12 +60,6 @@ export default function SchemeTableRow({ row, selected, onEditRow, onSelectRow, 
         </TableCell>
 
         <TableCell align="right" sx={{ px: 1, whiteSpace: 'nowrap' }}>
-          {/*<Tooltip title="Quick Edit" placement="top" arrow>*/}
-          {/*  <IconButton color={quickEdit.value ? 'inherit' : 'default'} onClick={quickEdit.onTrue}>*/}
-          {/*    <Iconify icon="solar:pen-bold" />*/}
-          {/*  </IconButton>*/}
-          {/*</Tooltip>*/}
-
           <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
             <Iconify icon="eva:more-vertical-fill" />
           </IconButton>
