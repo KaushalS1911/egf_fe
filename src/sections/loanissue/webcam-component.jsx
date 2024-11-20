8845d3e44435d50e6dffffa811c82571526218b0
KaushalS1911 <kaushalsojitra19@gmail.com>
2024-11-20 13:38:44 +0530

    check

--- src/sections/loanissue/webcam-component.jsx
+++ src/sections/loanissue/webcam-component.jsx
@@ -58,6 +58,7 @@ const WebcamComponent = () => {
             }}
           >
             Capture Photo
+
           </button>
           <button
             onClick={() => setIsCameraOpen(false)}
