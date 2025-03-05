ab89d406dac6a5ba39bea2021e3021aa58a7216a
Dax-savani <dakshsavani15@gmail.com>
2025-03-05 17:40:58 +0530

    next interest pay date issue

--- src/sections/loanpayhistory/view/interest-pay-details-form.jsx
+++ src/sections/loanpayhistory/view/interest-pay-details-form.jsx
@@ -140,10 +140,7 @@ function InterestPayDetailsForm({ currentLoan, mutate, configs }) {
       currentLoan?.issueDate && loanInterest?.length === 0
         ? new Date(currentLoan.issueDate)
         : new Date(loanInterest[0]?.to).setDate(new Date(loanInterest[0]?.to).getDate() + 1),
-    to:
-      new Date(currentLoan?.nextInstallmentDate) > new Date()
-        ? new Date(currentLoan.nextInstallmentDate)
-        : new Date(),
+    to:new Date(currentLoan.nextInstallmentDate),
     days: '',
     amountPaid: '',
     interestAmount: currentLoan?.scheme.interestRate || '',
